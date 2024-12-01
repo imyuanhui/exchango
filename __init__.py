9a45390fcc583cd4e3887b623aff40521dc65825
YuanhuiXu <xuuuyuanhui@foxmail.com>
2024-12-01 21:02:54 +0000

    refractor: database config

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from .api import fetch_rate
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    db = SQLAlchemy(app)
     
     @app.route("/")
     def home():
         return render_template("index.html")
+    
+    @app.route("/api/rates/<base>/<target>")
+    def get_rates(base=None, target=None):
+        return fetch_rate(base, target)
 
     return app
 
